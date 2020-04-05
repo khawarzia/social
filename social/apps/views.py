e5fc99bc0ae3f161012df674ece689ba298de71a
SardarDawar <49003336+SardarDawar@users.noreply.github.com>
2020-04-05 17:32:01 +0500

    Update views.py

--- social/apps/views.py
+++ social/apps/views.py
@@ -500,7 +500,7 @@ def add_to_insta(name,passs,post_content):
     chrome_options.add_experimental_option('excludeSwitches',['enable-automation'])
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
-    driver = webdriver.Chrome(executable_path=BASE_DIR + '/usr/bin/chromedriver',options=chrome_options)
+    driver = webdriver.Chrome(executable_path=BASE_DIR + '/usr/local/chromedriver',options=chrome_options)
 
     driver.get("https://www.instagram.com/accounts/login/?next=/explore/")
 
